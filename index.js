44c437e0a9a811e9f851eccf5ec22fb744bc5b8c
MichaelCoffey6 <coffeymichaelm@gmail.com>
2025-08-11 23:19:22 -0600

    Origin path fixed

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const contentTypes = {
 
 const requestListener = async (req, res) => {
   const { url } = req
-  console.log(await fs.readdir(`./`))
   const filePath = `.${url}` === "./"
     ? `./minecraft-skin-viewer/index.html`
     : `./minecraft-skin-viewer/${url}`
